982eca10aa3cf878693d3f309cc313426b1ca0ea
Edbri <brictp.12@gmail.com>
2025-07-23 11:40:15 -0400

    Initial nvim configuration

---
+++ init.lua
@@ -0,0 +1,5 @@
+require "config.settings"
+require "config.lazy"
+require "config.keymaps"
+require "config.cmp"
+require "config.lazy"require("lsp").config()
---
+++ lua/config/settings.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+vim.g.snippets = "luasnip"
+
+local opt = vim.o
+
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
